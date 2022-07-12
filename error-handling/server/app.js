206b367fe7592455edca6bf8a9df0534492d411f
pydovah37 <xterm.conf@gmail.com>
2022-07-12 12:11:42 -0700

    finished error-handling, added new short practice

--- error-handling/server/app.js
+++ error-handling/server/app.js
@@ -2,7 +2,29 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('GET / This is the root URL');
+  res.send('poop');
+});
+
+app.get('/dosomethingbad', (req, res) => {
+  let x = referenceThis + 5;
+  res.json('you will never do this');
+});
+
+app.use((req, res, next) => {
+  const error = new Error('You are screwed');
+  error.statusCode = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  if (err.statusCode === 404) {
+    res.json({ message: err.name, statusCode: err.message });
+  }
+
+  if (err.statusCode === 500) {
+    res.status(500);
+    res.json({ message: err.name, statusCode: err.message });
+  }
 });
 
 const port = 5000;
